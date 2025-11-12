5580a4dc7e10b279f26b0edfbab3e742e388bd84
mridul162 <mridulbd75@gmail.com>
2025-11-12 21:36:46 +0600

    update 10_functions.py file

--- python_beginner/10_functions.py
+++ python_beginner/10_functions.py
@@ -1,46 +1,47 @@
-# tom_exp_list = [2100, 3400, 3500]
-# joe_exp_list = [200, 500, 700]
+# An example of functions in Python
+tom_exp_list = [2100, 3400, 3500]
+joe_exp_list = [200, 500, 700]
 
-#########################################
-# total = 0
-# for item in tom_exp_list:
-#     total= total+item
-# print("Tom's total expenses: ", total)
-#
-# total = 0
-# for item in joe_exp_list:
-#     total= total+item
-# print("Joe's total expenses: ", total)
-#########################################
+# Without using functions, repeating same statements
+total = 0
+for item in tom_exp_list:
+    total= total+item
+print("Tom's total expenses: ", total)
 
-# def calculate_total(exp):
-#     total = 0
-#     for item in exp:
-#         total = total+item
-#     return total
-#
-# toms_total= calculate_total(tom_exp_list)
-# joes_total= calculate_total(joe_exp_list)
-#
-# print("Tom's total expenses: ", toms_total)
-# print("Joe's total expenses: ", joes_total)
+total = 0
+for item in joe_exp_list:
+    total= total+item
+print("Joe's total expenses: ", total)
 
-##########################################
-# total=0
+# Using functions to avoid repetition
+def calculate_total(exp):
+    total = 0
+    for item in exp:
+        total = total+item
+    return total
+
+toms_total= calculate_total(tom_exp_list)
+joes_total= calculate_total(joe_exp_list)
+
+print("Tom's total expenses: ", toms_total)
+print("Joe's total expenses: ", joes_total)
+
+# Function with default argument
+# total=0           # global variable
 def sum(a,b=0):
     """
     This function takes two arguments which are integer numbers,
     and it will return sum of them as an output.
     :param a:
     :param b:
-    :return:
+    :return: 
     """
     print("a: ", a)
     print("b: ", b)
-    total= a+b
-    print("Total Inside: ", total)
-    return total
+    total= a+b      # local variable
+    print("Total Inside: ", total) #
+    return total  
 
 n = sum(5)
 print("Total Outside: ", n)
-# print("Total Outside: ", total)
+print("Total Outside: ", total)
