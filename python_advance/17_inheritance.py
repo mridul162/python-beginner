8cd672ca910021f39723587cfa304095e7ecffa9
mridul162 <mridul162@gmail.com>
2025-11-20 21:57:47 +0600

    add comments explaining the inheritance exercise

--- python_advance/17_inheritance.py
+++ python_advance/17_inheritance.py
@@ -1,23 +1,25 @@
+# Inheritance Exercise
+
 class Vehicle:
-    def general_usage(self):
+    def general_usage(self):        # Method to print general usage
         print("general use: transportation")
 
-class Car(Vehicle):
-    def __init__(self):
+class Car(Vehicle):                 # Car class inherits from Vehicle
+    def __init__(self):             # Constructor
         print("I'm car")
         self.wheels = 4
         self.has_roof = True
 
-    def specific_usage(self):
+    def specific_usage(self):       # Method to print specific usage
         print("specific use: commute to work, vacation with family")
 
-class MotorCycle(Vehicle):
-    def __init__(self):
+class MotorCycle(Vehicle):          # MotorCycle class inherits from Vehicle
+    def __init__(self):             # Constructor
         print("I'm motor cycle")
         self.wheels = 2
         self.has_roof = False
 
-    def specific_usage(self):
+    def specific_usage(self):       # Method to print specific usage
         self.general_usage()
         print("specific use: road trip, racing")
 
